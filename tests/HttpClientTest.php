9b25dba03f112f4f68c4a879e7421f1f4163f257
iaintshine <bodziomista@gmail.com>
2016-02-25 23:37:34 +0100

    Properly encode DateTime objects in payloads

---
+++ tests/HttpClientTest.php
@@ -0,0 +1,40 @@
+<?php
+namespace BaseCRM;
+
+class HttpClientTest extends \PHPUnit_Framework_TestCase
+{
+  protected static $httpClient = null;
+
+  public static function setUpBeforeClass()
+  {
+    self::$httpClient = new HttpClient(new Configuration([
+      'accessToken' => str_repeat('1', 64) 
+    ]));
+  }
+
+  public function testDateTimeEncoding()
+  {
+    $payload = [
+      'updated_at' => new \DateTime('2016-02-25T01:02:03Z')
+    ];
+    
+    $result = PHPUnitUtil::callMethod(self::$httpClient, 'encodePayload', Array($payload));
+    $expectation = ['updated_at' => '2016-02-25T01:02:03+0000'];
+
+    $this->assertEquals($result, $expectation);
+  }
+
+  public function testNestedDateTimeEncoding()
+  {
+    $payload = [
+      'nested' => [
+        'updated_at' => new \DateTime('2016-02-25T01:02:03Z')
+      ],
+    ];
+
+    $result = PHPUnitUtil::callMethod(self::$httpClient, 'encodePayload', Array($payload));
+    $expectation = ['nested' => ['updated_at' => '2016-02-25T01:02:03+0000']];
+
+    $this->assertEquals($result, $expectation);
+  }
+}
---
+++ tests/PHPUnitUtil.php
@@ -0,0 +1,19 @@
+<?php
+namespace BaseCRM;
+
+class PHPUnitUtil
+{
+  public static function getPrivateMethod($obj, $name) 
+  {
+    $class = new \ReflectionClass($obj);
+    $method = $class->getMethod($name);
+    $method->setAccessible(true);
+    return $method;
+  }
+
+  public static function callMethod($obj, $name, array $args)
+  {
+    $method = self::getPrivateMethod($obj, $name);
+    return $method->invokeArgs($obj, $args);
+  }
+}
