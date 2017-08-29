50171a4ac0f3cc810aa8b986f6d0550f553e59c0
erwin <efedasz@gmail.com>
2017-08-29 09:49:19 +0200

    add tests for new resources

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -6,67 +6,92 @@ namespace BaseCRM;
  */
 class ClientTest extends TestCase
 {
-   
+
   public function testAccountPropertyExists()
   {
     $this->assertTrue(property_exists(self::$client, 'accounts'));
   }
-   
+
   public function testAssociatedContactPropertyExists()
   {
     $this->assertTrue(property_exists(self::$client, 'associatedContacts'));
   }
-   
+
   public function testContactPropertyExists()
   {
     $this->assertTrue(property_exists(self::$client, 'contacts'));
   }
-   
+
   public function testDealPropertyExists()
   {
     $this->assertTrue(property_exists(self::$client, 'deals'));
   }
-   
+
+  public function testDealSourcePropertyExists()
+  {
+   $this->assertTrue(property_exists(self::$client, 'dealSources'));
+  }
+
   public function testLeadPropertyExists()
   {
     $this->assertTrue(property_exists(self::$client, 'leads'));
   }
-   
+
+  public function testLeadSourcePropertyExists()
+  {
+   $this->assertTrue(property_exists(self::$client, 'leadSources'));
+  }
+
+  public function testLineItemPropertyExists()
+  {
+   $this->assertTrue(property_exists(self::$client, 'lineItems'));
+  }
+
   public function testLossReasonPropertyExists()
   {
     $this->assertTrue(property_exists(self::$client, 'lossReasons'));
   }
-   
+
   public function testNotePropertyExists()
   {
     $this->assertTrue(property_exists(self::$client, 'notes'));
   }
-   
+
+  public function testOrderPropertyExists()
+  {
+   $this->assertTrue(property_exists(self::$client, 'orders'));
+  }
+
   public function testPipelinePropertyExists()
   {
     $this->assertTrue(property_exists(self::$client, 'pipelines'));
   }
-   
+
+  public function testProductPropertyExists()
+  {
+   $this->assertTrue(property_exists(self::$client, 'products'));
+  }
+
   public function testSourcePropertyExists()
   {
     $this->assertTrue(property_exists(self::$client, 'sources'));
   }
-   
+
   public function testStagePropertyExists()
   {
     $this->assertTrue(property_exists(self::$client, 'stages'));
   }
-   
+
   public function testTagPropertyExists()
   {
     $this->assertTrue(property_exists(self::$client, 'tags'));
   }
-   
+
   public function testTaskPropertyExists()
   {
     $this->assertTrue(property_exists(self::$client, 'tasks'));
   }
-   
+
   public function testUserPropertyExists()
   {
     $this->assertTrue(property_exists(self::$client, 'users'));
